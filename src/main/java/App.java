f409cb5087edd704ce7177b16166a35ddfd746b7
Srushti <srushtilohar92@gmail.com>
2026-09-08 17:23:09 +0530

    SCRUM-7 Fix CI pipeline

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,14 +1,16 @@
 public class App {
 
     public static String login(String username, String password) {
+
         if (username.equals("admin") && password.equals("1234")) {
             return "Login successful";
         }
+
         return "Invalid username or password";
     }
 
     public static void main(String[] args) {
         System.out.println(login("admin", "1234"));
+        System.out.println("CI Pipeline Test Successful");
     }
 }
-System.out.println("CI Pipeline Test Successful");
